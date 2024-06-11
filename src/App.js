91a967639cd9fd0cf4cbd9df0676b687198baa84
Saksham Sachdeva <saksham5sachdeva@gmail.com>
2024-06-11 18:30:54 -0400

    restructured the files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login/login';
 import Layout from './components/Layout/Layout';
-import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { classes } from "./Layout.module.css";
-import Navigation from "../Navigation/Navigation";
+import Navigation from "../components/Navigation/Navigation";
 const Layout = ({children}) => {
   return (
     <div className={classes.container}>
