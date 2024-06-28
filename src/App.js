a9f92c7395bf989235683449cb4290d76464cc8e
Nitin Sharma <nitinsharma7699@gmail.com>
2024-06-28 10:19:53 -0400

    media queries added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigation from "./components/Navigation/Navigation";
 // import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import SearchUser from "./pages/user/user";
-import SignUp from "./pages/signup/signup";
+import SignUp from "./pages/CreateUser/createUser";
 import Page404 from './pages/page404/page404'
 import TreatmentPlanner from './pages/TreatmentPlanner/TreatmentPlanner';
 
@@ -12,8 +12,8 @@ import classes from "./App.module.css";
 import { Routes, Route } from "react-router-dom";
 import { Container, Grid } from "@mui/material";
   function App() {
-    const isAuthenticated = true;
-    const userType = 'user';
+    const isAuthenticated = true; // false for login page
+    const userType = 'user'; // admin for admin flow
     const AuthApp = (
       <Routes>
         <Route path="/" exact element={<Login />} />
