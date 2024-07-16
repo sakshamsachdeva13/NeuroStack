fea184d7638513e5f1f851ade022ae8154686b43
SKetulS <ketulshah3498@gmail.com>
2024-07-16 16:23:05 -0400

    Filters structure and Patient Files modified.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Container } from "@mui/material";
 
 import Login from "./pages/Login/login";
 import Navigation from "./components/Navigation/Navigation";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import Dashboard from './pages/Dashboard/Dashboard';
 import SearchUser from "./pages/user/user";
 import CreateUser from "./pages/CreateUser/createUser";
-import Page404 from "./pages/page404/page404";
-import TreatmentPlanner from "./pages/TreatmentPlanner/TreatmentPlanner";
+import Page404 from './pages/page404/page404';
+import TreatmentPlanner from './pages/TreatmentPlanner/TreatmentPlanner';
 
-import { Toaster } from "react-hot-toast"; // Importing Toaster from react-hot-toast
+import { Toaster } from 'react-hot-toast'; // Importing Toaster from react-hot-toast
 
 import classes from "./App.module.css";
 
 function App() {
-  let userDetails = sessionStorage.getItem("user");
-  const user = userDetails ? JSON.parse(userDetails) : null;
+  const user = sessionStorage.getItem("user")
+    ? JSON.parse(sessionStorage.getItem("user"))
+    : null;
 
   const userType = "admin";
   const AuthApp = (
@@ -37,16 +38,17 @@ function App() {
     </>
   );
 
+  const adminApp = (
+    <>
+      <Navigation userType={userType} />
+      <Routes>
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/" exact element={<SearchUser />} />
+        <Route path="/createUser" element={<CreateUser />} />
+      </Routes>
+    </>
+  );
 
-    const adminApp = (
-      <>
-        <Navigation userType ={userType}/>
-        <Routes>
-          <Route path='/' exact element={<SearchUser />} />
-          <Route path='/createUser' element={<CreateUser />} />
-        </Routes>
-      </>
-    )
   const renderApp = user
     ? userType === "admin"
       ? adminApp
@@ -58,7 +60,7 @@ function App() {
       <div>
         <Toaster /> {/* Rendering Toaster from react-hot-toast */}
       </div>
-      {renderApp}
+      {defaultApp}
     </Container>
   );
 }
